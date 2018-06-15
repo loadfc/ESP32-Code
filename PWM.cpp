07f5ce9b33203f8446d96dd422a7ea956ac7f519
khthana <khthana@hotmail.com>
2018-06-15 07:57:42 +0700

    Update PWM.cpp

--- PWM.cpp
+++ PWM.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
 
-int i = 0;
-
+int freq = 5000;
+int ledChannel = 0;
+int resolution = 8;
+ 
 void setup() {
-    ledcSetup(0, 5000, 13);
-    ledcAttachPin(LED_BUILTIN, 0);
+    ledcSetup(ledChannel, freq, resolution);
+    ledcAttachPin(LED_BUILTIN, ledChannel);
 }
-
+ 
 void loop() {
-    ledcWrite(0, i);
-    delay(20);
-    i = (i <= 8191) ? i+100 : 0;
+    for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){
+        ledcWrite(ledChannel, dutyCycle);
+        delay(7);
+    }
+    for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+        ledcWrite(ledChannel, dutyCycle);
+        delay(7);
+    }
 }
